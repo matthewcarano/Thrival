a8162703f6bbbfbd42d2244b8943f7bf004448f3
matthewcarano <matt@msquared.is>
2025-05-29 19:26:41 -0400

    Update thrival-system.tsx
        {/* Main Content */}

--- components/thrival-system.tsx
+++ components/thrival-system.tsx
@@ -980,6 +980,7 @@ const handleEditProgram = (programId: string, programs: any, setNewProgram: any,
       )}
 
       {/* Main Content */}
+      {/* DEBUG: Main tabs start here */}
       <div className="container mx-auto px-6 py-8">
         <Tabs value={activeTab} onValueChange={setActiveTab}>
           <TabsList className="grid w-full grid-cols-5">
